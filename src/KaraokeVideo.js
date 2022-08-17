d8f7c30377a82de2d139844913d8787b7212c33b
Kelly-Pinto <91966302+Kelly-Pinto@users.noreply.github.com>
2022-08-17 17:32:05 -0400

    Set up project. Added component for karaoke video.

---
+++ src/KaraokeVideo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./KaraokeVideo.css";
+
+export default function KaraokeVideo() {
+    return (
+        <div className="iframeContainer">
+          <div className="iframe">
+            <iframe allow="autoplay; gyroscope;" allowFullScreen height="100%" referrerPolicy="strict-origin" src="https://www.kapwing.com/e/62fbede838178e051d5a19e7" title="Embedded content made on Kapwing" width="100%"></iframe>
+          </div>
+        
+      </div>
+        
+    );
+}
