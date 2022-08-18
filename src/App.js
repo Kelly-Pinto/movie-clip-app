99346fc00e8e064f417d8479c711a5ee910758f3
Kelly-Pinto <91966302+Kelly-Pinto@users.noreply.github.com>
2022-08-17 21:24:41 -0400

    Installed bootstrap.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import KaraokeVideo from "./KaraokeVideo";
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       </header>
       <KaraokeVideo/>
+      </div>
     </div>
   );
 }
