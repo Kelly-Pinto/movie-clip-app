acbb88e6eab5f61ff0699e1f076b6d4384355379
Kelly-Pinto <91966302+Kelly-Pinto@users.noreply.github.com>
2022-10-17 10:20:06 -0400

    Replaced const audio with audio tag, added buttons

--- src/AudioSection.js
+++ src/AudioSection.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import "./AudioSection.css"; 
-import sound from "./final_62fe777cefb94100813195b5_557588.mp3";
    
 
 export default function AudioSection() {
-    const audio = new Audio(sound);
-   
   
-
-  const start = () => {
-    audio.play();
-  };
-
   return (
     <div>
-      <button onClick={start}>Play</button>
+      <audio src="./final_62fe777cefb94100813195b5_557588.mp3" preload="metadata"></audio>
+      <button>back 5 seconds</button>
+      <button>play / pause</button>
+      <button>forward 5 seconds</button>
+
+      {/*current time*/}
+      <div>0:00</div>
+      {/*progress bar*/}
+      <div>
+        <input type="range" />
+      </div>
+      {/*duration*/}
+      <div>2:49</div>
+
     </div>
   );
 }
