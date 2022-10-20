f0e15aad64b536417f6688dc9eddd0d98727ee0d
Kelly-Pinto <91966302+Kelly-Pinto@users.noreply.github.com>
2022-10-20 16:58:51 -0400

    Added audio control buttons

--- src/AudioSection.js
+++ src/AudioSection.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./AudioSection.css"; 
-   
+import {FiArrowLeftCircle} from "react-icons/fi";
+import {FiArrowRightCircle} from "react-icons/fi";  
+import {FiPlayCircle} from "react-icons/fi";
+import {FiPauseCircle} from "react-icons/fi";
 
 export default function AudioSection() {
-  
-  return (
-    <div>
-      <audio src="./final_62fe777cefb94100813195b5_557588.mp3" preload="metadata"></audio>
-      <button>back 5 seconds</button>
-      <button>play / pause</button>
-      <button>forward 5 seconds</button>
+  const [isPlaying, setIsPlaying] = useState(false);
+  const togglePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  }
 
-      {/*current time*/}
-      <div>0:00</div>
-      {/*progress bar*/}
-      <div>
-        <input type="range" />
+  return (
+    <div className="AudioSection">
+      <div className="AudioPlayers">
+        <audio src="./final_62fe777cefb94100813195b5_557588.mp3" preload="metadata"></audio>
+        <button className="audioControls"> <FiArrowLeftCircle /> 5 </button>
+        <button onClick={togglePlayPause} className="audioControls"> 
+          { isPlaying ? <FiPauseCircle /> : <FiPlayCircle /> }
+          </button>
+        <button className="audioControls"> 5 <FiArrowRightCircle /> </button>
+
+        {/*current time*/}
+        <div>0:00</div>
+        {/*progress bar*/}
+        <div>
+          <input type="range" />
+        </div>
+        {/*duration*/}
+        <div>2:49</div>
+      
       </div>
-      {/*duration*/}
-      <div>2:49</div>
 
     </div>
   );
